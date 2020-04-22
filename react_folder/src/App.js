e354daac07741d6af6ce42edc3845753f5ca6bb9
Ally Brannon <allison.brannon@gmail.com>
2020-04-22 14:57:59 -0400

    Updated folder names in App.js

--- react_folder/src/App.js
+++ react_folder/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+<<<<<<< HEAD
 import Intro from "./components/Intro";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import SignupPage from "./components/SignupPage";
+=======
+import Intro from "./Components/Intro";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+>>>>>>> Updated folder names in App.js
 import "./App.css";
 
 function App() {
